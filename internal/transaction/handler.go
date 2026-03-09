262fb725165942986772667afa37a51acfb0a3d2
abrshDev <abrshmelkamu3@gmail.com>
2026-03-09 21:59:57 +0300

    feat(transaction): implement deposit HTTP handler

--- internal/transaction/handler.go
+++ internal/transaction/handler.go
@@ -1,1 +1,39 @@
 package transaction
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(service *Service) *Handler {
+	return &Handler{service: service}
+}
+
+type DepositRequest struct {
+	Amount int64 `json:"amount"`
+}
+
+func (h *Handler) Deposit(c *fiber.Ctx) error {
+
+	var body DepositRequest
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).
+			JSON(fiber.Map{"error": "invalid request"})
+	}
+
+	userID := c.Locals("user_id").(uint)
+
+	err := h.service.Deposit(userID, body.Amount)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).
+			JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "deposit successful",
+	})
+}
