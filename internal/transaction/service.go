ca0c11409fc4e8e6861364d279a287f08547ce8f
abrshDev <abrshmelkamu3@gmail.com>
2026-03-09 10:22:32 +0300

    Transaction service

--- internal/transaction/service.go
+++ internal/transaction/service.go
@@ -1,1 +1,15 @@
 package transaction
+
+import "github.com/abrshDev/ledger-system/internal/wallet"
+
+type Service struct {
+	repo      *Repository
+	walletSvc *wallet.Service
+}
+
+func NewService(repo *Repository, walletSvc *wallet.Service) *Service {
+	return &Service{
+		repo:      repo,
+		walletSvc: walletSvc,
+	}
+}
