5e1dbf24cd77bcba2cc3696d4289d8d497931764
abrshDev <abrshmelkamu3@gmail.com>
2026-03-13 15:40:21 +0300

    modified the ledger model

--- internal/ledger/model.go
+++ internal/ledger/model.go
@@ -3,10 +3,10 @@ package ledger
 import "time"
 
 type LedgerEntry struct {
-	ID        uint `gorm:"primaryKey"`
-	WalletID  uint
-	Type      string
-	Amount    int64
-	Reference string
-	CreatedAt time.Time
+	ID            uint `gorm:"primaryKey"`
+	WalletID      uint
+	TransactionID uint
+	Type          string `gorm:"type:varchar(10)"`
+	Amount        int64
+	CreatedAt     time.Time
 }
